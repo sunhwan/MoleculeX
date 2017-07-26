4acda899b8c657d0265240697660fd9177a31edc
Sunhwan Jo <sunhwan@silcsbio.com>
2017-07-26 00:04:06 -0400

    Add atomic element class

--- moleculex/atom.py
+++ moleculex/atom.py
@@ -1,4 +1,16 @@
+from .element import Element
+
 class Atom:
     """Atom"""
 
-    pass
+    name = None
+    element = None
+
+    def __init__(self, attr_dict=None, **kwargs):
+        for k in kwargs:
+            self.k = kwargs[k]
+
+        if 'symbol' in kwargs or 'number' in kwargs:
+            symbol = kwargs.get('symbol', None)
+            number = kwargs.get('number', None)
+            self.element = Element(symbol=symbol, number=number)
