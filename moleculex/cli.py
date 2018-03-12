688618b1f42af0b148cdc9e337ec4d31611de437
Sunhwan Jo <sunhwan@silcsbio.com>
2018-03-12 00:57:11 -0400

    add cli

---
+++ moleculex/cli.py
@@ -0,0 +1,25 @@
+import argparse
+import sys
+
+from .molecule import Molecule
+
+def pdb2rtf(args):
+    mol = Molecule.from_pdb(open(args.input))
+
+def run(argv=sys.argv[1:]):
+    parser = argparse.ArgumentParser()
+
+    subparsers = parser.add_subparsers()
+    subparsers.required = True
+    subparsers.dest = 'command'
+
+    pconvert = subparsers.add_parser('pdb2rtf')
+    pconvert.add_argument('input')
+    pconvert.add_argument('--output', default="output")
+    pconvert.set_defaults(func=pdb2rtf)
+
+    args = parser.parse_args()
+    args.func(args)
+
+if __name__ == '__main__':
+    run()
--- moleculex/molecule.py
+++ moleculex/molecule.py
@@ -1,5 +1,7 @@
 import networkx as nx
 
+from .atom import Atom
+
 class Molecule():
     """Molecule class"""
 
@@ -25,3 +27,51 @@ class Molecule():
         """Return a list of bonds"""
 
         return self.graph.edges()
+
+    @staticmethod
+    def from_pdb(pdbfile):
+        molecule = None
+        in_chain = False
+        current_chain = None
+        current_residue = None
+        for line in pdbfile:
+            token = line[:6].strip()
+            if not in_chain and (token in ('ATOM', 'HETATM')):
+                in_chain = True
+                if molecule is None:
+                    molecule = Molecule()
+                    atomDict = {}
+                else:
+                    raise NotImplementedError('Parsing multiple molecule in a single PDB file is not implemented yet.')
+            if in_chain and token == 'TER':
+                in_chain = False
+
+            if in_chain and (token in ('ATOM', 'HETATM')):
+                number = int(line[6:11])
+                name = line[11:16].strip()
+                altloc = line[16:17].strip()
+                resname = line[17:20].strip()
+                chainid = line[21:22]
+                resnr = int(line[22:26])
+                x = float(line[30:38])
+                y = float(line[38:46])
+                z = float(line[46:54])
+                occupancy = float(line[54:60])
+                bfactor = float(line[60:66])
+                segid = line[72:76].strip()
+
+                if altloc and altloc != 'A':
+                    continue
+
+                atom = Atom(name=name)
+                atomDict[number] = atom
+                molecule.add_atom(atom)
+
+            if token == 'CONECT':
+                entries = [int(_) for _ in line.strip().split()[1:]]
+                atom1 = atomDict[entries[0]]
+                for i in range(1, len(entries)):
+                    atom2 = atomDict[entries[i]]
+                    molecule.add_bond(atom1, atom2)
+
+        return molecule
